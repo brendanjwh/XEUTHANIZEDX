518c13819f4f894359eeb18ac67c59915471ebc3
Brendan White <eggrollcontrol@gmail.com>
2017-11-06 09:45:56 -0600

    resized images for background

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -6,26 +6,26 @@ export default React.createClass({
   render() {
     const images = [
       "/public/img/live/1.JPG",
-      "/public/img/live/2.JPG",
-      "/public/img/live/3.JPG",
-      "/public/img/live/4.JPG",
-      "/public/img/live/5.JPG",
-      "/public/img/live/6.JPG",
-      "/public/img/live/7.JPG",
-      "/public/img/live/8.JPG",
-      "/public/img/live/9.JPG",
-      "/public/img/live/10.JPG",
-      "/public/img/live/11.JPG",
-      "/public/img/live/12.JPG",
-      "/public/img/live/13.JPG",
-      "/public/img/live/14.JPG",
-      "/public/img/live/15.JPG",
-      "/public/img/live/16.JPG",
-      "/public/img/live/17.JPG",
-      "/public/img/live/18.JPG",
-      "/public/img/live/19.JPG",
-      "/public/img/live/20.JPG",
-      "/public/img/live/21.JPG"
+      // "/public/img/live/2.JPG",
+      // "/public/img/live/3.JPG",
+      // "/public/img/live/4.JPG",
+      // "/public/img/live/5.JPG",
+      // "/public/img/live/6.JPG",
+      // "/public/img/live/7.JPG",
+      // "/public/img/live/8.JPG",
+      // "/public/img/live/9.JPG",
+      // "/public/img/live/10.JPG",
+      // "/public/img/live/11.JPG",
+      // "/public/img/live/12.JPG",
+      // "/public/img/live/13.JPG",
+      // "/public/img/live/14.JPG",
+      // "/public/img/live/15.JPG",
+      // "/public/img/live/16.JPG",
+      "/public/img/live/17.PNG",
+      "/public/img/live/18.JPG"
+      // "/public/img/live/19.JPG",
+      // "/public/img/live/20.JPG",
+      // "/public/img/live/21.JPG"
     ];
 
     return (
