7e9d0811568aa8eee9bd7b9915d3583ba25751bb
Brendan White <eggrollcontrol@gmail.com>
2017-11-11 12:57:03 -0600

    better bandcamp links...background on music down.  using flexbox

---
+++ helmswald-post/readme.txt
@@ -0,0 +1,26 @@
+OTF: Helmswald Post
+Dennis Ludlow 2017 all rights reserved
+by Sharkshock 
+dennis@sharkshock.net
+
+Helmswald Post is a handsome Blackletter that's been years in the making. After Enchanted Land was created the idea for a medieval script with a more modern feel materialized.
+There's a large mix of wispy terminals, flambouyant caps, and the use of negative space to create contrast.
+Elements from many different letters and time periods make their way into this beautiful display font. Basic latin, extended latin, punctuation, ligatures, European accents, diacritics,
+alternates, and Cyrillic characters are included in the full version. There are many blackletter fonts out there but very few with Cyrillic characters for Russian, Ukranian, or Macedonian. 
+Romanized Capitals and alternates are included by using the alternates or stylistic sets. Old world numerals may be substituted by accessing the stylistic sets as well. The demo version
+features basic latin, basic punctuation, and limited kerning. Use Helmswald Post for a beer logo, poster, or book cover.
+
+Complete regular version is available with purchase of commercial license or $25 payment via PayPal. Just use the donation button on our homepage at www.sharkshock.net.
+Please note that this $25 is for PERSONAL use only and does NOT constitute a commercial license. 
+
+This font like my others are free for personal use only as long as this readme file stays intact. For commercial use please contact me at dennis@sharkshock.net to discuss an end user license agreement. You can also visit www.sharkshock.net/license for more info and terms. I also design custom 
+fonts for companies, logos, and many other things. If you'd like to leave me a PayPal donation you can use my email address above. Your generosity will be most appreciated! 
+
+Thank you for your support!
+
+visit www.sharkshock.net for more and take a bite out of BORING design!
+
+tags: Europe, medieval, blackletter, black, serif, ancient, modern, style, Russian, Russia, Cyrillic, accents, European, diacritics, Old, English, script, Sharkshock, ligatures, alternates,
+beer, poster, company, logo, old world, England, pen, strokes
+
+
--- js/Music.jsx
+++ js/Music.jsx
@@ -1,40 +1,44 @@
 import React from "react";
+import Img from "react-image";
+
+const BackgroundImage = () => <Img src="../public/img/live/2.JPG" />;
 
 const Music = () => (
-  <div>
-    <iframe
-      id="live-sessions"
-      className="bandcamp"
-      src="https://bandcamp.com/EmbeddedPlayer/album=4156429228/size=large/bgcol=333333/linkcol=ffffff/transparent=true/"
-      seamless
-    >
-      <a href="http://xeuthanizedx.bandcamp.com/album/live-sessions">
-        LIVE SESSIONS by XEUTHANIZEDX
-      </a>
-    </iframe>
+  <div className="image-div">
+    <div className="bandcamp-embed">
+      <iframe
+        id="live-sessions"
+        className="bandcamp"
+        src="https://bandcamp.com/EmbeddedPlayer/album=4156429228/size=medium/bgcol=333333/tracklist=true/linkcol=ffffff/transparent=true/"
+        seamless
+      >
+        <a href="http://xeuthanizedx.bandcamp.com/album/live-sessions">
+          LIVE SESSIONS by XEUTHANIZEDX
+        </a>
+      </iframe>
 
-    <iframe
-      id="swing-on-sight"
-      className="bandcamp"
-      src="https://bandcamp.com/EmbeddedPlayer/album=1521845450/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/"
-      seamless
-    >
-      <a href="http://xeuthanizedx.bandcamp.com/album/swing-on-sight">
-        SWING ON SIGHT by XEUTHANIZEDX
-      </a>
-    </iframe>
+      <iframe
+        id="swing-on-sight"
+        className="bandcamp"
+        src="https://bandcamp.com/EmbeddedPlayer/album=1521845450/size=medium/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/"
+        seamless
+      >
+        <a href="http://xeuthanizedx.bandcamp.com/album/swing-on-sight">
+          SWING ON SIGHT by XEUTHANIZEDX
+        </a>
+      </iframe>
 
-    <iframe
-      id="suffer"
-      className="bandcamp"
-      src="https://bandcamp.com/EmbeddedPlayer/album=957328478/size=large/bgcol=333333/linkcol=ffffff/transparent=true/"
-      seamless
-    >
-      <a href="http://xeuthanizedx.bandcamp.com/album/suffer">
-        SUFFER by XEUTHANIZEDX
-      </a>
-    </iframe>
-
+      <iframe
+        id="suffer"
+        className="bandcamp"
+        src="https://bandcamp.com/EmbeddedPlayer/album=957328478/size=medium/bgcol=333333/linkcol=ffffff/transparent=true/"
+        seamless
+      >
+        <a href="http://xeuthanizedx.bandcamp.com/album/suffer">
+          SUFFER by XEUTHANIZEDX
+        </a>
+      </iframe>
+    </div>
   </div>
 );
 
